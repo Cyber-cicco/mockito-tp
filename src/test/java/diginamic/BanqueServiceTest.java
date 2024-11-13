b77f44c91d980d96679fe9cc80740418864ef9c5
Abel Ciccoli <abelcoli@outlook.fr>
2024-11-13 17:20:07 +0100

    feat(tp2):  ajout du tp2

--- src/test/java/diginamic/BanqueServiceTest.java
+++ src/test/java/diginamic/BanqueServiceTest.java
@@ -4,6 +4,7 @@ import diginamic.entites.Compte;
 import diginamic.exception.BanqueException;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,5 +35,53 @@ class BanqueServiceTest {
         } catch (BanqueException e) {
             System.out.println("shouldn't be here");
         }
+    }
+
+    @Test
+    void deposer() {
+        var compte = new Compte("1312", "abelcoli@outlook.fr", 200d);
+        var processor = Mockito.spy(TransactionProcessor.class);
+        var compteDao = Mockito.mock(CompteDao.class);
+        service.setTransactionProcessor(processor);
+        service.setCompteDao(compteDao);
+        Mockito.doNothing().when(processor).envoyerMailConfirmation(Mockito.any(Compte.class), Mockito.anyString());
+        service.deposer(compte, 200d);
+        assertEquals(compte.getSolde(), 400d);
+
+
+    }
+
+    @Test
+    void retirer() {
+        var compte = new Compte("1312", "abelcoli@outlook.fr", 200d);
+        var processor = Mockito.spy(TransactionProcessor.class);
+        var compteDao = Mockito.mock(CompteDao.class);
+        var transacDao = Mockito.mock(TransactionDao.class);
+        Mockito.doNothing().when(processor).envoyerMailConfirmation(Mockito.any(Compte.class), Mockito.anyString());
+        service.setTransactionProcessor(processor);
+        service.setCompteDao(compteDao);
+        service.setTransactionDao(transacDao);
+        service.retirer(compte, 100d);
+        Mockito.doNothing().when(processor).envoyerMailConfirmation(Mockito.any(Compte.class), Mockito.anyString());
+        assertEquals(compte.getSolde(), 100d);
+        service.retirer(compte, 300d);
+        assertNotEquals(processor.getErrors().size(), 0);
+        assertEquals(compte.getSolde(), 100d);
+    }
+
+    @Test
+    void virer() {
+        var compte1 = new Compte("1312", "abelcoli@outlook.fr", 200d);
+        var compte2 = new Compte("1315", "abelcoli@gmail.fr", 200d);
+        var processor = Mockito.spy(TransactionProcessor.class);
+        var compteDao = Mockito.mock(CompteDao.class);
+        var transacDao = Mockito.mock(TransactionDao.class);
+        Mockito.doNothing().when(processor).envoyerMailConfirmation(Mockito.any(Compte.class), Mockito.anyString());
+        service.setTransactionProcessor(processor);
+        service.setCompteDao(compteDao);
+        service.setTransactionDao(transacDao);
+        service.virer(compte1, compte2, 50d);
+        assertEquals(compte1.getSolde(), 149.5d);
+        assertEquals(compte2.getSolde(), 250d);
     }
 }
